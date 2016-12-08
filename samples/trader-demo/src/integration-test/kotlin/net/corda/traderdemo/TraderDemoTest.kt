e27cd718638c5c560db9671a335ded6929ef49e4
josecoll <jose.coll@r3cev.com>
2016-12-08 10:28:18 +0000

    Fixed TraderDemo integration text (added new BankOfCorda node)

--- samples/trader-demo/src/integration-test/kotlin/net/corda/traderdemo/TraderDemoTest.kt
+++ samples/trader-demo/src/integration-test/kotlin/net/corda/traderdemo/TraderDemoTest.kt
@@ -1,8 +1,11 @@
 package net.corda.traderdemo
 
+import net.corda.bank.flow.IssuerFlow
 import net.corda.core.getOrThrow
 import net.corda.core.node.services.ServiceInfo
 import net.corda.node.driver.driver
+import net.corda.node.services.User
+import net.corda.node.services.startFlowPermission
 import net.corda.node.services.transactions.SimpleNotaryService
 import net.corda.testing.getHostAndPort
 import org.junit.Test
@@ -14,6 +17,8 @@ class TraderDemoTest {
             val nodeA = startNode("Bank A").getOrThrow()
             val nodeAApiAddr = nodeA.config.getHostAndPort("webAddress")
             val nodeBApiAddr = startNode("Bank B").getOrThrow().config.getHostAndPort("webAddress")
+            val user = User("user1", "test", permissions = setOf(startFlowPermission<IssuerFlow.IssuanceRequester>()))
+            startNode("BankOfCorda", rpcUsers = listOf(user)).getOrThrow()
 
             assert(TraderDemoClientApi(nodeAApiAddr).runBuyer())
             assert(TraderDemoClientApi(nodeBApiAddr).runSeller(counterparty = nodeA.nodeInfo.legalIdentity.name))
